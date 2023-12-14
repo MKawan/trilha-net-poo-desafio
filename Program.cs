2e4a9d016fde161e5a27ca962fefd71b7ce9c2d0
Mateus Kawan <mateus77kawan@gmail.com>
2023-12-14 16:56:09 -0300

    desafio concluido com sucesso

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia(numero: "62997856921", modelo: "Modelo 1", imei: "494949494191919", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Cod");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Iphone iphone = new Iphone(numero: "61989572514", modelo: "Modelo 2", imei: "444449494494948", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Warface");
+
